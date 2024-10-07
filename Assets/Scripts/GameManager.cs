a5c35f9c821b1cae9572c1f23098a215350ca7e7
Artem Melnykov <artemmelnykov29@gmail.com>
2024-10-07 02:44:50 +0200

    Added a score text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float _spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
+    private int _score;
 
 
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        _score = 0;
+        scoreText.text = "Score: " + _score;
     }
 
     IEnumerator SpawnTarget()
