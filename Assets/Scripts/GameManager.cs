5b8b9d03aa6230b5dff80e7fd7e087f3e7105e72
Artem Melnykov <artemmelnykov29@gmail.com>
2024-10-12 09:40:08 +0200

    Cleaned up code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,49 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public List<GameObject> targets;
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI gameOverText;
-    public Button restartButton;
-    public GameObject gameMenu;
+    [SerializeField]
+    private List<GameObject> _targets;
+    [SerializeField]
+    private TextMeshProUGUI _scoreText;
+    [SerializeField]
+    private TextMeshProUGUI _gameOverText;
+    [SerializeField]
+    private Button _restartButton;
+    [SerializeField] 
+    private GameObject _gameMenu;
     private float _spawnRate = 1.0f;
     private int _score;
 
     public bool isGameActive;
-
-    IEnumerator SpawnTarget()
-    {
-        while (isGameActive)
-        {
-            yield return new WaitForSeconds(_spawnRate);
-            int index = Random.Range(0, targets.Count);
-            Instantiate(targets[index]);
-        }
-    }
-
-    public void UpdateScore(int scoreToAdd)
-    {
-        _score += scoreToAdd;
-        scoreText.text = "Score: " + _score;
-    }
-
-    public void GameOver()
-    {
-        restartButton.gameObject.SetActive(true);
-        gameOverText.gameObject.SetActive(true);
-        isGameActive = false;
-    }
-
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
 
     public void StartGame(Difficulty difficulty)
     {
@@ -54,6 +31,34 @@ public class GameManager : MonoBehaviour
         UpdateScore(0);
 
         _spawnRate /= (int)difficulty;
-        gameMenu.gameObject.SetActive(false);
+        _gameMenu.gameObject.SetActive(false);
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(_spawnRate);
+            int index = Random.Range(0, _targets.Count);
+            Instantiate(_targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        _score += scoreToAdd;
+        _scoreText.text = "Score: " + _score;
+    }
+
+    public void GameOver()
+    {
+        _restartButton.gameObject.SetActive(true);
+        _gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
